4389e34b8bc88d37a6a64f9eee843eedd1a21558
abdfnx <abdfnabd@gmail.com>
2021-04-03 11:00:56 +0300

    Deploy Secman

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const debug = process.env.NODE_ENV !== "production";
-
-module.exports = {
-  assetPrefix: !debug ? "/" : "",
-};
--- pages/api/routes.ts
+++
@@ -1,6 +0,0 @@
-export const Routes = {
-  secman: "https://secman.vercel.app",
-  secmanOrg: "https://github.com/secman-team",
-  secmanRepo: "https://github.com/secman-team/secman",
-  statusSite: "https://secman.checklyhq.com",
-};
