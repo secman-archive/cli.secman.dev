8ff62ecc05ad4ce63880b18c4547ac978b627240
abdfnx <abdfnabd@gmail.com>
2021-04-07 14:19:52 +0300

    x

--- out/_next/static/DSlSqI9QOI0Qk9Du5IbJq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-c16d7c96f99be74374cc.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-00951ae22ab0f1bec160.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
