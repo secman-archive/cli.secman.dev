17dbf0787399522012fc0bf18b02a8b7711d1cef
abdfnx <abdfnabd@gmail.com>
2021-04-08 19:04:54 +0300

    Deploy Secman

---
+++ out/_next/static/FjXtYUTfgthQ_xABcY_cD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-5fb5df53b73ac30fa753.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-00951ae22ab0f1bec160.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
