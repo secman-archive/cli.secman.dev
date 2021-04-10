a62f9df7ecabd53d0567da0446ef57c5d2c263bc
Timothee <80710181+Timothee-Cardoso@users.noreply.github.com>
2021-04-10 12:28:01 +0300

    Update routes.ts

--- pages/api/routes.ts
+++ pages/api/routes.ts
@@ -1,9 +1,53 @@
-export const Routes = {
-  secmanSite: "https://secman.vercel.app",
-  secmanOrg: "https://github.com/secman-team",
-  secmanRepo: "https://github.com/secman-team/secman",
-  statusSite: "https://secman.checklyhq.com",
-  logoURL: "https://imgs-secman.web.app/logo.png",
-  smURL: "https://imgs-secman.web.app/sm.png",
-  gsmURL: "https://imgs-secman.web.app/glitsh_sm.png"
+export const core = "https://secman.vercel.app";
+
+export const URLS = {
+  gh: "https://github.com",
+
+  secman_org: "",
+
+  secman_repo: "",
+
+  sm_scoop_repo: "",
+
+  docs: {
+    main: `${core}/docs`,
+    sm_sync: "",
+    cryptography: "",
+    commands: "",
+  },
+
+  posts: `${core}/posts`,
+
+  cli: `${core}/cli`,
+
+  changelog: `${core}/changelog`,
+
+  team: `${core}/team`,
+
+  contact: `${core}/contact`,
+
+  getting_started: {
+    main: `${core}/getting_started`,
+  },
+
+  installation: {
+    main: `${core}/installation`,
+  },
+
+  discord: "https://discord.gg/secman",
+
+  features: {
+    id: `${core}/#features`,
+  },
+
+  deps_site: "https://secman-team.github.io",
+
+  sys_statuses: "https://secman.checklyhq.com",
 };
+
+URLS.secman_org = `${URLS.gh}/secman-team`;
+URLS.secman_repo = `${URLS.secman_org}/secman`;
+URLS.docs.sm_sync = `${URLS.docs.main}/secman-sync`;
+URLS.docs.cryptography = `${URLS.docs.main}/cryptography`;
+URLS.docs.commands = `${URLS.docs.main}/commands`;
+URLS.sm_scoop_repo = `${URLS.secman_org}/sm-scoop`;
