6b236269b2238cc9977f7ad8023c4f851d3b0f9e
abdfnx <abdfnabd@gmail.com>
2021-06-01 11:08:02 +0300

    update the installers with the new version `v5.3.62`

--- public/install.sh
+++ public/install.sh
@@ -11,7 +11,7 @@ rmOldFiles() {
 
 v=$(curl --silent "https://api.github.com/repos/secman-team/secman/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-releases_page=https://github.com/secman-team/secman/releases/download
+releases_api_url=https://github.com/secman-team/secman/releases/download
 
 successInstall() {
     echo "yesss, secman was installed successfully 😎, you can type secman --help"
@@ -21,37 +21,45 @@ installSecman() {
     echo "installing secman..."
 
     if [ "$UNAME" == "Linux" ]; then
-        smUrl=$releases_page/$v/secman_deb_${v}_x64.zip
+        name="secman_linux_${v}_amd64"
+        smUrl=$releases_api_url/$v/$name.zip
 
         wget $smUrl
-        sudo chmod 755 secman_deb_${v}_x64.zip
-        unzip secman_deb_${v}_x64.zip
-        rm secman_deb_${v}_x64.zip
+        sudo chmod 755 $name.zip
+        unzip $name.zip
+        rm $name.zip
+
+        # secman
+        sudo mv $name/bin/secman $smLocLD
+        sudo mv $name/share/man/man1/secman.1 /usr/share/man/man1
+
+        rm $name
 
     elif [ "$UNAME" == "Darwin" ]; then
-        smUrl=$releases_page/$v/secman_macos_${v}_x64.zip
+        name="secman_macos_${v}_amd64"
+        smUrl=$releases_api_url/$v/$name.zip
 
         wget $smUrl
-        sudo chmod 755 secman_macos_${v}_x64.zip
-        unzip secman_macos_${v}_x64.zip
-        rm secman_macos_${v}_x64.zip
+        sudo chmod 755 $name.zip
+        unzip $name.zip
+        rm $name.zip
+
+        # secman
+        sudo mv $name/bin/secman $smLocLD
+        sudo mv $name/share/man/man1/secman.1 /usr/share/man/man1
+
+        rm $name
     fi
 
-    # secman
-    sudo mv secman_bin/secman $smLocLD
-    sudo mv secman_share/man/man1/secman*.1.gz /usr/share/man/man1
-
     # chmod
-    sudo chmod 755 $smLocLD/secman*
-
-    rm -rf secman_bin secman_share LICENSE
+    sudo chmod 755 $smLocLD/secman
 }
 
 mainCheck() {
     if [ -x "$(command -v git)" ]; then
         installSecman
     else
-        echo "You Should Install Git"
+        echo "You Need to Install Git"
     fi
 }
 
