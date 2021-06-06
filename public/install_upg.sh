49d39cddd9b263f2337f87ad7d8dfb28c271707f
abdfnx <abdfnabd@gmail.com>
2021-06-06 13:14:32 +0300

    add `-rf` to remove commands, create upgrade scripts

---
+++ public/install_upg.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+smLocLD=/usr/local/bin
+UNAME=$(uname)
+
+v=$(curl --silent "https://api.github.com/repos/secman-team/secman/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+releases_api_url=https://github.com/secman-team/secman/releases/download
+
+if [ "$UNAME" == "Linux" ]; then
+    name="secman_linux_${v}_amd64"
+    smUrl=$releases_api_url/$v/$name.zip
+
+    wget $smUrl
+    sudo chmod 755 $name.zip
+    unzip $name.zip
+    rm $name.zip
+
+    # secman
+    sudo mv $name/bin/secman $smLocLD
+    sudo mv $name/share/man/man1/secman.1 /usr/share/man/man1
+
+    rm -rf $name
+
+elif [ "$UNAME" == "Darwin" ]; then
+    name="secman_macos_${v}_amd64"
+    smUrl=$releases_api_url/$v/$name.zip
+
+    wget $smUrl
+    sudo chmod 755 $name.zip
+    unzip $name.zip
+    rm $name.zip
+
+    # secman
+    sudo mv $name/bin/secman $smLocLD
+    sudo mv $name/share/man/man1/secman.1 /usr/share/man/man1
+
+    rm -rf $name
+fi
+
+# chmod
+sudo chmod 755 $smLocLD/secman
